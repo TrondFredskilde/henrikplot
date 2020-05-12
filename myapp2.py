6e810ee516e657c3871b4e6ca68362c8971b65a4
TrondFredskilde <61452448+TrondFredskilde@users.noreply.github.com>
2020-05-12 15:41:06 +0200

    Add files via upload

--- myapp2.py
+++ myapp2.py
@@ -1,21 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 12 10:20:46 2020
 
+@author: henri
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 11 01:33:48 2020
+
+@author: henri
+"""
 
 # data load
 import numpy as np
 import pandas as pd
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from datetime import datetime
-#import calendar
+import calendar
+import math
 
 df = pd.read_csv("us_states_covid19_daily.csv",index_col=False)
-
 df = df.set_index(pd.to_datetime(df['date'], format="%Y%m%d"))
 
 states = {
         'AK': 'Alaska',
         'AL': 'Alabama',
         'AR': 'Arkansas',
-        'AS': 'American Samoa',
         'AZ': 'Arizona',
         'CA': 'California',
         'CO': 'Colorado',
@@ -24,7 +35,6 @@ states = {
         'DE': 'Delaware',
         'FL': 'Florida',
         'GA': 'Georgia',
-        'GU': 'Guam',
         'HI': 'Hawaii',
         'IA': 'Iowa',
         'ID': 'Idaho',
@@ -39,7 +49,6 @@ states = {
         'MI': 'Michigan',
         'MN': 'Minnesota',
         'MO': 'Missouri',
-        'MP': 'Northern Mariana Islands',
         'MS': 'Mississippi',
         'MT': 'Montana',
         'NC': 'North Carolina',
@@ -62,9 +71,7 @@ states = {
         'TX': 'Texas',
         'UT': 'Utah',
         'VA': 'Virginia',
-        'VI': 'Virgin Islands',
         'VT': 'Vermont',
-        'WA': 'Washington',
         'WI': 'Wisconsin',
         'WV': 'West Virginia',
         'WY': 'Wyoming'
@@ -78,19 +85,31 @@ df = df[['state', 'positive', 'negative',
            'deathIncrease',
            'hospitalizedIncrease',
            ]]
-
+import datetime
 df_full = pd.DataFrame()
 for state in list(states.keys()): #list(states.keys()) #["NY"]
     df_state = df[df.state==state]
     df_state = df_state.sort_index()
-    weeks = range(0,int(len(df_state)/7)*7,7)
+    weeks = range(0,int(1+len(df_state)/7)*7,7)
     df_state_week = df_state.iloc[weeks,:]
     df_state_week["new_cases"] = (df_state_week.positive-df_state_week.positive.shift(periods=1)).to_numpy()
     df_state_week["date_string"] = df_state_week.index.strftime('%Y-%m-%d')
     df_state_week["week_number"] = range(len(df_state_week))
     df_state_week["new_cases"][0]=1
-    
-    
+    df_state_week["color"] = 'blue'
+    df_state_week["change"] = df_state_week.new_cases.pct_change()
+    df_state_week["death_change"] = df_state_week.death.pct_change()
+    # add two new predictions
+    for i in range(2):
+        temp = (df_state_week.iloc[-1])
+        temp.change=df_state_week.iloc[-1].change*0.80+df_state_week.iloc[-2].change*0.2
+        temp.new_cases = math.ceil(temp.new_cases*(1+temp.change))
+        temp.positive = temp.positive+temp.new_cases
+        temp.week_number = temp.week_number + 1
+        temp.death = temp.death + math.ceil(temp.death*(temp.death_change))
+        temp.color = "red"
+        temp.date_string = str(df_state_week.index[-1] + datetime.timedelta(days=7))[:10]
+        df_state_week.loc[df_state_week.index[-1] + datetime.timedelta(days=7)] = temp
     df_full = df_full.append(df_state_week)
     
     
@@ -112,19 +131,23 @@ source = ColumnDataSource(data={'positive': df_full[df_full.state==state].iloc[:
                                 'new_cases': df_full[df_full.state==state].iloc[:(N+2)].new_cases.tolist(),
                                "date_string" : df_full[df_full.state==state].iloc[:(N+2)].date_string.tolist(),
                                 "week_number" : df_full[df_full.state==state].iloc[:(N+2)].week_number.tolist(),
-                                "death" : df_full[df_full.state==state].iloc[:(N+2)].death.tolist()
+                                "death" : df_full[df_full.state==state].iloc[:(N+2)].death.tolist(),
+                                "color_test" : df_full[df_full.state==state].iloc[:(N+2)].color.tolist()
                                })    
 
     
     
 # Create a figure with x_axis_type='datetime': p
-p = figure(title = "COVID-19 development in each US state",plot_width=700,plot_height=400,x_axis_type='log',y_axis_type='log', x_axis_label='Total confirmed cases', y_axis_label='New weekly cases')
+p = figure(tools = "",title = "COVID-19 development in each US state",plot_width=700*2,plot_height=400*2,x_axis_type='log',y_axis_type='log', x_axis_label='Total confirmed cases', y_axis_label='New weekly cases')
 
 # Plot date along the x-axis and price along the y-axis
-p.line(x='positive', y='new_cases', source=source)
+p.line(x='positive', y='new_cases',source=source)
 
 # Add circle glyphs to the figure p
-p.circle(x='positive', y='new_cases', size=8, source=source)
+p.circle(x='positive', y='new_cases',fill_color="color_test", size=8, source=source,legend = "Observed")
+
+p.circle(x=[0], y=[0],fill_color="red", size=8,legend = "Prediction")
+
 
 # import hoverTool form bokeh.models
 from bokeh.models import HoverTool
@@ -137,7 +160,7 @@ p.add_tools(hover)
 
 from bokeh.models import ColumnDataSource, Slider
 
-slider = Slider(start=0,end=8,value=N+1,step=1,title="Week")
+slider = Slider(start=0,end=12,value=N+1,step=1,title="Week")
 
 # Define a callback function: callback
 def callback(attr, old, new):
@@ -150,7 +173,8 @@ def callback(attr, old, new):
                                 'new_cases': df_full[df_full.state==state].iloc[:N].new_cases.tolist(),
                                "date_string" : df_full[df_full.state==state].iloc[:N].date_string.tolist(),
                                 "week_number" : df_full[df_full.state==state].iloc[:N].week_number.tolist(),
-                                "death" : df_full[df_full.state==state].iloc[:N].death.tolist()
+                                "death" : df_full[df_full.state==state].iloc[:N].death.tolist(),
+                                "color_test" : df_full[df_full.state==state].iloc[:(N)].color.tolist()
                                 }
 
 # Attach the callback to the 'value' property of slider
@@ -168,14 +192,17 @@ menu = Select(options=list(states.values()),value= "New York",title = "Select St
 
 def callback2(attr, old, new):
     state = states_short[menu.value]
-    N = slider.value
+    N = slider.value + 1 
     source.data = {'positive': df_full[df_full.state==state].iloc[:N].positive.tolist(),
                                 'new_cases': df_full[df_full.state==state].iloc[:N].new_cases.tolist(),
                                "date_string" : df_full[df_full.state==state].iloc[:N].date_string.tolist(),
                                 "week_number" : df_full[df_full.state==state].iloc[:N].week_number.tolist(),
-                                "death" : df_full[df_full.state==state].iloc[:N].death.tolist()
+                                "death" : df_full[df_full.state==state].iloc[:N].death.tolist(),
+                                "color_test" : df_full[df_full.state==state].iloc[:(N)].color.tolist()
                                 }
 menu.on_change("value",callback2)
+
+p.legend.location = "top_left"
 
 from bokeh.io import curdoc
 from bokeh.layouts import column
